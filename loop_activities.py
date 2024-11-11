182ad3138537e86c2fe1ce9406d33d77bbc08a89
Sam Crook <sjpcrook@outlook.com>
2024-11-11 12:31:03 +0000

    loop activities

---
+++ loop_activities.py
@@ -0,0 +1,35 @@
+#Activity 1
+
+# for i in range(13):
+#     print("hello world")
+#     #print(i)
+
+# total = 0
+# while total < 13:
+#     print("hello world")
+#     #print(total)
+#     total = total + 1
+
+#Activity 2
+
+# for i in range(1, 13):
+#     for j in range(1, 13):
+#         print(f"{i} times {j} is {i*j}")
+
+
+#Activity 3
+attempts_left = 3
+while True:
+    answer = input("Please enter your password:  > ")
+    attempts_left = attempts_left - 1
+    if answer == "St@t1st1cs":
+        print("Successfully logged in.")
+        break
+    elif attempts_left <= 0:
+        print("You have been locked out.")
+        break
+    else:
+        print(f"Password Incorrect. You have {attempts_left} attempt(s) left.")
+
+        
+
