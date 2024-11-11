894abc511803d8469cd04bbb028617b707adf6cd
Sam Crook <sjpcrook@outlook.com>
2024-11-11 11:54:12 +0000

    loop notes

---
+++ loop_notes.py
@@ -0,0 +1,21 @@
+fav_drinks = [
+    "latte",
+    "mocha",
+    "americano"
+]
+# print(len(fav_drinks))
+
+# for drink in fav_drinks:
+#     print(drink)
+
+# # \n - new line
+# for i in range :
+#     print("hello, world! \nhow are you?")
+#     print(f"{i} \n")
+
+#sum of 100
+sum = 0
+for i in range(1, 101):
+    sum = sum + i
+print(sum)
+
